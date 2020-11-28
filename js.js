b21d789c721c26b8380f68c5da6e2c084792e04c
chamodya wimansha <chamodyawimansha@gmail.com>
2020-11-28 15:20:09 +0530

    urls added

--- js.js
+++ js.js
@@ -8,13 +8,17 @@ const chart = d3
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom + 50);
 
+let kickStarterPledgesUrl =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json";
+let movieSalesUrl =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
+let videoGameSalesUrl =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
+
 Promise.all([
-  d3.json(
-    "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
-  ),
-  d3.json(
-    "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"
-  ),
+  d3.json(kickStarterPledgesUrl),
+  d3.json(movieSalesUrl),
+  d3.json(videoGameSalesUrl),
 ])
   .then((data) => {})
   .catch((error) => {
