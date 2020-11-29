4a107bf50ab5db5301227d27982212436a9cbfa9
chamodya wimansha <chamodyawimansha@gmail.com>
2020-11-29 11:23:23 +0530

    map and colors added

--- js.js
+++ js.js
@@ -1,26 +1,38 @@
-const margin = { top: 50, right: 50, bottom: 50, left: 50 };
-const width = 960 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
+const width = 1260;
+const height = 600;
 
 const chart = d3
   .select("#chart-container")
   .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom + 50);
+  .attr("width", width)
+  .attr("height", height + 50);
 
-let kickStarterPledgesUrl =
-  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json";
-let movieSalesUrl =
-  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
-let videoGameSalesUrl =
-  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
+d3.json(
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json"
+)
+  .then((data) => {
+    let treemap = d3.treemap().size([width, height]).padding(1);
+    let root = d3.hierarchy(data).sum((d) => d.value);
+    treemap(root);
 
-Promise.all([
-  d3.json(kickStarterPledgesUrl),
-  d3.json(movieSalesUrl),
-  d3.json(videoGameSalesUrl),
-])
-  .then((data) => {})
+    const colors = d3.scaleOrdinal().range(colorbrewer.Dark2[6]);
+
+    chart
+      .selectAll("g")
+      .data(root.leaves())
+      .enter()
+      .append("g")
+      .attr("transform", (d) => "translate(" + d.x0 + "," + d.y0 + ")")
+      .append("rect")
+
+      .attr("width", function (d) {
+        return d.x1 - d.x0;
+      })
+      .attr("height", function (d) {
+        return d.y1 - d.y0;
+      })
+      .style("fill", (d) => colors(d.data.category));
+  })
   .catch((error) => {
     if (error) throw error;
   });
